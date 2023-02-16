069177e1b73d1ebb28ec711a203ad5b8340d6ff1
buenSurfAdmin <buensurfadm@gmail.com>
2023-02-16 10:36:11 +0000

    Creacion renting y consigna(inventario),TipoClase(calendario)

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\Odoo 15.0.20230202\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files\Odoo 15.0.20230202\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=admin
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\Odoo 15.0.20230202\PostgreSQL\share\locale
+
+                            
